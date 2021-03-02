25027cb8b005d9b37e328a8cea7896ddf4174fa5
John Henson <jhensonfsu@gmail.com>
2021-03-02 18:47:19 -0500

    testing out lifecylce support

--- src/main/java/com/idealfed/forms/api/rest/LifeCycleController.java
+++ src/main/java/com/idealfed/forms/api/rest/LifeCycleController.java
@@ -1,7 +1,9 @@
 package com.idealfed.forms.api.rest;
 
+import com.atlassian.connect.spring.AtlassianHostUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -11,15 +13,15 @@ public class LifeCycleController {
     private static final Logger log = LoggerFactory.getLogger(LifeCycleController.class);
 
     @RequestMapping(value="/installed2", method = RequestMethod.POST)
-    public static String installApp(@RequestBody String payload, HttpServletResponse response) {
-        log.info("Installing app.  Payload: " + payload);
+    public static String installApp(@AuthenticationPrincipal AtlassianHostUser hostUser, @RequestBody String payload, HttpServletResponse response) {
+        log.info("hostUser: " + hostUser+"; Installing app.  Payload: " + payload);
         response.setStatus(200);
         return "OK";
     }
 
     @RequestMapping(value="/uninstalled2", method = RequestMethod.POST)
-    public static String uninstallApp(@RequestBody String payload, HttpServletResponse response) {
-        log.info("Uninstalling app.  Payload: " + payload);
+    public static String uninstallApp(@AuthenticationPrincipal AtlassianHostUser hostUser, @RequestBody String payload, HttpServletResponse response) {
+        log.info("hostUser: " + hostUser+"; Uninstalling app.  Payload: " + payload);
         response.setStatus(200);
         return "OK";
     }
