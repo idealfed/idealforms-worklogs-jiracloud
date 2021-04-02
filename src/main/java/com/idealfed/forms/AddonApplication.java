58be0d2486c8d0e268637774335dae5c5a185e61
John Henson <jhensonfsu@gmail.com>
2021-04-02 16:53:12 -0400

    Revert "updates for jdh dev"
    This reverts commit 4d6ccb3cf4a1deb209b2afa07020302032f2bbe8.
    
    # Conflicts:
    #	src/main/java/com/idealfed/forms/AddonApplication.java
    #	src/main/resources/atlassian-connect.json

--- src/main/java/com/idealfed/forms/AddonApplication.java
+++ src/main/java/com/idealfed/forms/AddonApplication.java
@@ -1,47 +1,20 @@
 package com.idealfed.forms;
 
 import com.idealfed.forms.repositories.*;
-import org.hibernate.service.spi.InjectService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.Arrays;
-
-@Configuration
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = {FormsetRepository.class, SnippetRepository.class, CustomTypeRepository.class, FormRepository.class, VersionRepository.class})
 public class AddonApplication {
 
-    @Autowired
-    private Environment environment;
-
-    @Value("${ijfRoot}")
-    public static String appRoot = "/dev20";
-
-    private static Logger log = LoggerFactory.getLogger(AddonApplication.class);
+    //@Value("${ijfRoot}")
+    public static final String appRoot = "/dev20";
     public static final String productName = "iftWorklogs";
 
     public static void main(String[] args) throws Exception {
-        log.debug("App root: "+appRoot);
         new SpringApplication(AddonApplication.class).run(args);
-    }
-
-    @PostConstruct
-    public void initApplication() throws IOException {
-        if (environment.getActiveProfiles().length == 0) {
-            log.warn("No Spring profile configured.  Running with default configuration.");
-        } else {
-            log.info("Running with Spring profile(s): {}", Arrays.toString(environment.getActiveProfiles()));
-        }
     }
 }
