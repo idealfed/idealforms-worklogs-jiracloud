89f88a467b80a89b80a1acf8a654541a7142d30a
John Henson <jhensonfsu@gmail.com>
2021-07-17 10:21:49 -0400

    final hsts configuration

--- src/main/java/com/idealfed/forms/config/WebSecurityConfig.java
+++ src/main/java/com/idealfed/forms/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 @EnableWebSecurity
@@ -12,13 +13,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.headers()
                 .httpStrictTransportSecurity()
                 .includeSubDomains(true)
                 .preload(true)
                 .maxAgeInSeconds(31536000)
                 .requestMatcher(r -> true);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
         http.headers().contentTypeOptions();
         http.headers().frameOptions().disable();
         http.headers().referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.ORIGIN_WHEN_CROSS_ORIGIN);
